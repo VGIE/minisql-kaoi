e78f62719a17e450b60e056521a16458a5875790
kevin <kevinurilop11@gmail.com>
2026-03-05 09:52:24 +0100

    # 5 regular expressions for the match

--- DBManager/Parser/MiniSQLParser.cs
+++ DBManager/Parser/MiniSQLParser.cs
@@ -9,19 +9,19 @@ namespace DbManager
         public static MiniSqlQuery Parse(string miniSQLQuery)
         {
             //TODO DEADLINE 2
-            const string selectPattern = null;
+            const string selectPattern = @"^SELECT\s+(?P<columns>[A-Za-z][A-Za-z0-9]*(?:\s*,\s*[A-Za-z][A-Za-z0-9]*)*|\*)\s+FROM\s+(?P<table>[A-Za-z][A-Za-z0-9]*)(?:\s+WHERE\s+(?P<condition>.+))?\s*;?\s*$";
             
-            const string insertPattern = null;
+            const string insertPattern = @"^INSERT\s+INTO\s+(?P<table>[A-Za-z][A-Za-z0-9]*)\s+VALUES\s*\((?P<values>.+)\)\s*;?\s*$";
             
-            const string dropTablePattern = null;
+            const string dropTablePattern = @"^DROP\s+TABLE\s+(?P<table>[A-Za-z][A-Za-z0-9]*)\s*;?\s*$";
             
             //Note: The parsing of CREATE TABLE should accept empty columns "()"
             //And then, an execution error should be given if a CreateTable without columns is executed
-            const string createTablePattern = null;
+            const string createTablePattern = @"^CREATE\s+TABLE\s+(?P<table>[A-Za-z][A-Za-z0-9]*)\s*\((?P<column_defs>[A-Za-z][A-Za-z0-9]*\s+(?:INT|DOUBLE|TEXT)(?:\s*,\s*[A-Za-z][A-Za-z0-9]*\s+(?:INT|DOUBLE|TEXT))*)\)\s*;?\s*$";
             
-            const string updateTablePattern = null;
+            const string updateTablePattern = @"^UPDATE\s+(?P<table>[A-Za-z][A-Za-z0-9]*)\s+SET\s+(?P<set_columns>[A-Za-z][A-Za-z0-9]*\s*=\s*[^,]+(?:\s*,\s*[A-Za-z][A-Za-z0-9]*\s*=\s*[^,]+)*)\s+WHERE\s+(?P<condition>.+)\s*;?\s*$";
             
-            const string deletePattern = null;
+            const string deletePattern = @"^DELETE\s+FROM\s+(?P<table>[A-Za-z][A-Za-z0-9]*)\s+WHERE\s+(?P<condition>.+)\s*;?\s*$";
             
 
             //TODO DEADLINE 4
@@ -36,14 +36,14 @@ namespace DbManager
             const string addUserPattern = null;
             
             const string deleteUserPattern = null;
-            
+
 
             //TODO DEADLINE 2
             //Parse query using the regular expressions above one by one. If there is a match, create an instance of the query with the parsed parameters
             //For example, if the query is a "SELECT ...", there should be a match with selectPattern. We would create and return an instance of Select
             //initialized with the table name, the columns, and (possibly) an instance of Condition.
             //If there is no match, it means there is a syntax error. We will return null.
-
+            
             //TODO DEADLINE 4
             //Do the same for the security queries (CREATE SECURITY PROFILE, ...)
             
