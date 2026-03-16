fbcda191957f8daeba1189c3fd7cbf99e9ccfb6f
Igor <icastano012@ikasle.ehu.eus>
2026-03-16 11:20:01 +0100

    #6 Delete Execute method done, with 3 respective tests created

--- DBManager/Parser/Delete.cs
+++ DBManager/Parser/Delete.cs
@@ -12,16 +12,22 @@ namespace DbManager.Parser
         public Delete(string table, Condition where)
         {
             //TODO DEADLINE 2: Initialize member variables
-            
+            Table = table;
+            Where = where;
         }
 
         public string Execute(Database database)
         {
             //TODO DEADLINE 3: Run the query and return the appropriate message
-            //DeleteSuccess or the last error in the database
-            
-            return null;
-            
+            //DeleteSuccess or the last error in the database           
+            Table tabla = database.TableByName(Table);
+            if (database==null)
+                return Constants.Error;
+            else if (tabla == null)
+                return Constants.TableDoesNotExistError;
+            else if(database.DeleteWhere(Table, Where))
+                return Constants.DeleteSuccess;
+            return Constants.Error;          
         }
     }
 }
