425b9ed3184d37be14c46e5459691082b5dddf68
oierv <ovillalba003@ikasle.ehu.eus>
2026-04-13 11:36:28 +0200

    #8 Manager IsGrantedPrivilege done but not the test

--- DBManager/Security/Manager.cs
+++ DBManager/Security/Manager.cs
@@ -73,7 +73,19 @@ namespace DbManager.Security
         {
             //TODO DEADLINE 5: Return true if the username has this privilege on this table. False otherwise (also in case of error)
             
-            return false;
+            if(username == null || table == null || privilege == null)
+                return false;
+            
+
+            Profile profile = ProfileByUser(username);
+
+            if(profile == null)
+                return false;
+            
+            if(profile.Name == Profile.AdminProfileName)
+                return true;
+
+            return profile.IsGrantedPrivilege(table, privilege);
             
         }
 
