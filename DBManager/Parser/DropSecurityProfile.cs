664db1b16148d9a0c0da0c33e4c61e52328fd938
kevin <kevinurilop11@gmail.com>
2026-05-06 18:48:26 +0200

    #8 drop security profile 1 try

--- DBManager/Parser/DropSecurityProfile.cs
+++ DBManager/Parser/DropSecurityProfile.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 using DbManager.Parser;
+using DbManager.Security;
 
 namespace DbManager
 {
@@ -20,7 +21,25 @@ namespace DbManager
         {
             //TODO DEADLINE 5: Run the query and return the appropriate message
             //UsersProfileIsNotGrantedRequiredPrivilege, SecurityProfileDoesNotExistError, DropSecurityProfileSuccess
+            if(database == null)
+            {
+                return Constants.Error;
+            }
             
+            else if (database.IsUserAdmin() == false)
+            {
+                return Constants.UsersProfileIsNotGrantedRequiredPrivilege;
+            }
+            Profile p = database.SecurityManager.ProfileByName(ProfileName);
+            if (p == null)
+            {
+                return Constants.SecurityProfileDoesNotExistError;
+            }
+            bool result = database.SecurityManager.RemoveProfile(ProfileName);
+            if (result == true)
+            {
+               return Constants.DropSecurityProfileSuccess; 
+            }
             return null;
             
         }
